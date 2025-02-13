dd98b294b19658c23dba769769422b46bd8f03be
kozae00 <rhwogus777@gmail.com>
2025-02-13 12:02:59 +0900

    data rendering

--- frontend/app/post/list/page.tsx
+++ frontend/app/post/list/page.tsx
@@ -1,29 +1,26 @@
 export default async function Page() {
-    // api 호출
-  
-    const response = await fetch("http://localhost8080/api/v1/posts");
-     
-    if(!response.ok) {
-      throw new Error("에러");
-    }
-  
-    const rsData = await response.json();
-  
-    console.log(rsData.code);
-    console.log(rsData.msg);
-    console.log(rsData.data);
-  
-    return (
-      <div>
-        <h1>글 목록.</h1>
-  
-        <hr />
-  
-        <ul>
-          <li>글1</li>
-          <li>글2</li>
-        </ul>
-      </div>
-    );
+  // api 호출
+
+  const response = await fetch("http://localhost:8080/api/v1/posts");
+
+  if (!response.ok) {
+    throw new Error("에러");
   }
-  
+
+  const rsData = await response.json();
+
+  return (
+    <div>
+      <h1>글 목록.</h1>
+      <div>응답 코드 : {rsData.code}</div>
+      <div>결과 메시지 : {rsData.msg}</div>
+
+      <hr />
+
+      <ul>
+        <li>글1</li>
+        <li>글2</li>
+      </ul>
+    </div>
+  );
+}
