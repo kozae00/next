245ffa49d34a7f92fea42f28ae146343090c2329
kozae00 <rhwogus777@gmail.com>
2025-02-13 12:17:37 +0900

    post list rendergin

--- frontend/app/post/list/page.tsx
+++ frontend/app/post/list/page.tsx
@@ -1,6 +1,4 @@
 export default async function Page() {
-  // api 호출
-
   const response = await fetch("http://localhost:8080/api/v1/posts");
 
   if (!response.ok) {
@@ -8,18 +6,35 @@ export default async function Page() {
   }
 
   const rsData = await response.json();
+  const pageDto = rsData.data;
 
   return (
     <div>
-      <h1>글 목록.</h1>
+      <h1>글 목록</h1>
+
       <div>응답 코드 : {rsData.code}</div>
       <div>결과 메시지 : {rsData.msg}</div>
+
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
 
       <hr />
 
       <ul>
-        <li>글1</li>
-        <li>글2</li>
+        {pageDto.items.map((item: any) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {item.published}</div>
+              <div>listed : {item.listed}</div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
