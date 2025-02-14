1e17b8afb8e1da81abd083ec7c63a606d87fc528
kozae00 <rhwogus777@gmail.com>
2025-02-14 14:50:35 +0900

    my info server fetch

--- frontend/app/member/me/page.tsx
+++ frontend/app/member/me/page.tsx
@@ -1,5 +1,19 @@
+import { cookies } from "next/headers";
 import ClientPage from "./ClientPage";
+import client from "@/src/lib/backend/client";
 
 export default async function Page() {
-  return <ClientPage />;
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  if (response.error) {
+    return <div>{response.error.msg}</div>;
+  }
+
+  const memberDto = response.data.data;
+
+  return <ClientPage me={memberDto} />;
 }
