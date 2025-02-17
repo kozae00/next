fffb1fc131055b585b3df64c0076a2f08c373f37
kozae00 <rhwogus777@gmail.com>
2025-02-17 12:07:41 +0900

    spring cookie to browser

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,23 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
+import client from "./lib/backend/client";
+import { cookies } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log("middleware");
+export async function middleware(request: NextRequest) {
+  const reqToken = request.cookies.get("accessToken");
+  const nextResponse = NextResponse.next();
+
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  const springCookie = response.response.headers.getSetCookie();
+  nextResponse.headers.set("set-cookie", String(springCookie));
+
+  return nextResponse;
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/:path*",
 };
