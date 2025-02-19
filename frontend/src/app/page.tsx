e027b656795ed8d340d0aa4a2a3a3d20dba9bd1d
kozae00 <rhwogus777@gmail.com>
2025-02-19 10:01:25 +0900

    shadcn button test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>메인페이지입니다.</div>;
+  return (
+    <>
+      <div>메인페이지입니다.</div>
+      <Button variant="outline">Button1</Button>
+      <Button variant="destructive">Button2</Button>
+      <Button className="bg-red-300 w-[100px]">Button3</Button>
+    </>
+  );
 }
